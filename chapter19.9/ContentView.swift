49db16edf3a83953bca68d233184d8f21a6b88b3
heroparkjisung2 <heroparkjisung2@gmail.com>
2020-06-09 19:54:35 +0900

    new

--- chapter19.9/ContentView.swift
+++ chapter19.9/ContentView.swift
@@ -10,7 +10,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 10){
+            Text("Hello, World!")
+            .modifier(StandTitle())
+            Text("Hello, World!")
+            .modifier(StandTitle())
+        }
+    }
+}
+
+// 커스텀 수정자
+// 나만의 커스텀 수정자: 이것은 뷰에 자주 적용되는 대표적인 수정자들을 갖고자 할때 특히 유용하다.
+// 만약 ViewModifier 프로토콜을 따르는 구조체로 선언 했다면, 구문안의 body만 입력해도 자동으로 func 구문이 완성된다!
+
+struct StandTitle:ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 0.2)
+            .shadow(color: Color.yellow, radius: 5, x:0, y:10)
     }
 }
 
